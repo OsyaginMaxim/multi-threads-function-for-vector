c8bce2f49af042468c3a7e72fa1a028f737c1b2f
Maxim Osyagin <i.am.dream5896@gmail.com>
2017-12-24 01:47:43 +0300

    work

--- main.cpp
+++ main.cpp
@@ -15,20 +15,23 @@ void myVecFunc(Iter begin, Iter end, const int& num, Func func){
     for(int i = 0; i < num; i++) {
         threads[i] = std::thread([&begin, end, func](){
             Iter it;
-            while(begin != end){
+            while(begin != end) {
                 {
                     std::lock_guard<std::mutex> lockMut(mut);
                     it = begin;
                     begin++;
                 }
                 func(*it);
+                /*std::thread::id my_ID = std::this_thread::get_id();
+                std::cout<< my_ID << "hello" <<std::endl;*/
             }
-
         });
 
     }
     for(int i = 0; i < num; i++){
-        threads[i].join();
+        if(threads[i].joinable()) {
+            threads[i].join();
+        }
     }
 }
 
@@ -38,10 +41,10 @@ void myVecFunc(Iter begin, Iter end, const int& num, Func func){
 int main() {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dist(1, 20);
-    std::uniform_real_distribution<> dist2(1,20);
-    int size = 5;
-    int countOfThread = 3;
+    std::uniform_int_distribution<> dist(10, 50);
+    std::uniform_real_distribution<> dist2(10,20);
+    int size = 30;
+    int countOfThread = 5;
     std::vector<int> vector1(size);
     for(int i = 0; i < size; i++){
         vector1[i] = dist(gen);
@@ -50,22 +53,23 @@ int main() {
     for(auto& v : vector2){
         v = dist2(gen);
     }
-    for(auto& v : vector2){
-        std::cout<<v<<" ";
-    }
-    std::cout<<std::endl;
     for(auto& v : vector1){
         std::cout<<v<<" ";
     }
     std::cout<<std::endl;
+
     myVecFunc( vector1.begin(), vector1.end(),countOfThread, [](int& n ){
         n++;
-        std::cout<< std::to_string(n) + "\n";
+        std::cout<< std::to_string(n) + " ";
     });
+    std::cout<<std::endl;
+    for(auto& v : vector2){
+        std::cout<<v<<" ";
+    }
     std::cout<<std::endl;
     myVecFunc( vector2.begin(),vector2.end(), countOfThread, [](double& n){
         n--;
-        std::cout<< std::to_string(n) + "\n";
+        std::cout<< std::to_string(n) + " ";
     });
     std::cout<<std::endl;
 
